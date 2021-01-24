62cf1e0f3b666120e17e59e8d330db8a85a8126e
depaeu-nbu <pdqbv104uan4626@126.com>
2021-01-24 17:28:39 +0800

    实验二第一次

--- java2/src/java2/Main.java
+++ java2/src/java2/Main.java
@@ -1,10 +1,19 @@
 package java2;
-
+import java.util.*;
 public class Main {
+			public static String getBirthDate(String id)
+			{
+				String Year = id.substring(6,10);
+				String Month = id.substring(10,12);
+				String Day = id.substring(12,14);
+				return(Year+"-"+Month+"-"+Day);
+			}
+			public static void main(String aegs[])
+			{
+				System.out.println("please input ID:");
+				Scanner in = new Scanner(System.in);
+				String ID = in.nextLine();
+				System.out.println(getBirthDate(ID));
+			}
+		}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
