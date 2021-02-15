f4f634e5d610649e39477d0a46828b81fe38c04a
depaeu-nbu <pdqbv104uan4626@126.com>
2021-02-15 17:05:29 +0800

    实验五第一次

--- java/src/java/Main.java
+++ java/src/java/Main.java
@@ -1,83 +1,51 @@
 package java;
-
+import java.util.Arrays;
 import java.util.Scanner;
-class Person
-{
-private String name;
-private int age;
-private boolean gender;
-private int id;
-public Person()
-{
- System.out.println("This is constructor");
- System.out.printf("%s,%d,%s,%d\n",name,age,gender,id);
- 
+class PersonSortable implements Comparable<PersonSortable> {
+    private String name;
+    private int age;
+    public PersonSortable(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    public String toString()
+    {
+        return name+'-'+age;
+    }
+    @Override
+    public int compareTo(PersonSortable o) {
+        int x = this.name.compareTo(o.name);
+        if(x!=0)
+            return x;
+        else return this.getAge()-o.getAge();    
+    }
+    
 }
 
-public Person(String name,int age,boolean gender)
-{
- this.name =name;
- this.age =age;
- this.gender =gender;
-}
 
-public String toString()
-{
- return String.format("Person [name=%s, age=%d, gender=%s, id=%d]",name,age,gender,id);
-}
-
-public void setName(String name)
-{
- this.name =name;
-}
-public String getName()
-{
- return name;
-}
-public void setAge(int age)
-{
- this.age =age;
-}
-public int getAge()
-{
- return age;
-}
-public void setGender(boolean gender)
-{
- this.gender =gender;
-}
-public boolean getGender()
-{
- return gender;
-}
-public void setId(int id)
-{
- this.id =id;
-}
-public int getId()
-{
- return id;
-}
-
-}
 public class Main {
-public static void main(String[] args) {
- // TODO Auto-generated method stub
- Scanner in=new Scanner(System.in);
- int n=Integer.parseInt(in.nextLine());
- Person[] persons=new Person[n];
- //System.out.println(Arrays.toString(persons));
- for(int i=0;i<persons.length;i++)
- {
-  Person person=new Person(in.next(),in.nextInt(),in.nextBoolean());
-  
-  persons[i]=person;
- }
- for(int j=persons.length-1;j>=0;j--)
- {
-  System.out.println(persons[j]);
- }
- Person p=new Person();
- System.out.print(p.toString());
-}
-}
+    
+
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        while (cin.hasNextInt()) {
+            int n = cin.nextInt();
+            PersonSortable[] s = new PersonSortable[n];
+            for (int i = 0; i < n; i++) {
+                s[i] = new PersonSortable(cin.next(),cin.nextInt());
+            }
+            Arrays.sort(s);
+            for (PersonSortable t : s) {
+                System.out.println(t);
+            }
+            System.out.println(Arrays.toString(PersonSortable.class.getInterfaces()));
+            }
+        }
+
+    }
